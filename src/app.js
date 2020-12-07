c9db2243d990ad0b96978eae681a53e8bd7fe8fa
Fampo Jonas <jojostra92@gmail.com>
2020-12-07 12:14:41 +0000

    Mise à jours du dépot: Réorganisation des fichiers pour le responsive design.

---
+++ src/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// JAVASCRIP FILE
+
+const navBar = document.querySelector('#header-nav-bar')
+const navbarHeight = navBar.offsetHeight
+const menuButton = document.querySelector('#menu-button')
+
+const handleScrollY = function () {
+    let supportPageOffset = window.pageXOffset !== undefined
+    let isCSS1Compat = ((document.compatMode || "") === "CSS1Compat")
+
+    return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop
+}
+
+function navbarStiky() {
+    navBar.classList.toggle('stiky', window.scrollY && handleScrollY() > navbarHeight)
+}
+
+window.onload = () => {}
